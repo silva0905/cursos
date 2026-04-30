3b81e5c70f34b730201246d8a6ba359b369e2d54
ThiagoRibeiro <tg259325@gmail.com>
2026-04-30 17:44:02 -0300

    Desafio 3 guanabara

---
+++ #desafio 2.py
@@ -0,0 +1,6 @@
+#desafio 2
+#criar um script python que leia o dia, o mes  e o ano de nascimento de uma pessoa e mostrar uma mensagem com data formatada
+dia = input('Dia em que voce nasceu = ' )
+mes = input('Mes em que voce nasceu = ' )
+ano = input('Ano em que voce nasceu =  ')
+print('Voce nasceu no dia',dia, 'de', mes,"no ano de ",ano,"." " Correto?" )
---
+++ #desafio 3.py
@@ -0,0 +1,5 @@
+#desafio 3
+# criar um scrpit que leia dois numeros  e tente mostrar a soma entre eles
+n1 = int(input("Digite um numero: "))
+n2 = int(input('Digite um segundo numero: '))
+print('A soma entre esses numeros é: ', n1 + n2)
